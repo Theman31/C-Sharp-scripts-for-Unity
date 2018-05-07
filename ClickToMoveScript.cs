a2ac01726fa11d37e565796348da517dce67cf79
Eli Dery <elidery@Ye-Olde-Macbook-Pro.local>
2018-05-07 17:22:31 -0600

    added a click to move script that also interacts with game components, and changed the name of the clorchanging file

---
+++ ClickToMoveScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+    NavMeshAgent playerAgent;
+
+	// Use this for initialization
+	private void Start () {
+        playerAgent = GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
+	}
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactable Object")
+            {
+                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+            }
+            else
+            {
+                playerAgent.stoppingDistance = 0;
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+}
