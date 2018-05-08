00fd7df4fa85da23a9a16be4cd81b17e06778b88
Eli Dery <elidery@Ye-Olde-Macbook-Pro.local>
2018-05-07 18:10:09 -0600

    Added and Interaction script, as well as a base action and item interaction script. created files for an NPC script, and created a readme

---
+++ Interactable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour {
+    public NavMeshAgent playerAgent;
+
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent)
+    {
+        this.playerAgent = playerAgent;
+        playerAgent.stoppingDistance = 2f;
+        playerAgent.destination = this.transform.position;
+
+        Interact();
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with base class");
+    }
+}
